010e1ea95fa69ee64a37d537662ab7e84a4fc00c
unknown <abdulatifsh90@gmail.com>
2026-03-20 19:09:46 +0900

    background jobs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve ??= {};
+    config.resolve.alias ??= {};
+
+    config.resolve.alias["@mediapipe/tasks-vision$"] = path.resolve(
+      process.cwd(),
+      "node_modules/@mediapipe/tasks-vision/vision_bundle.mjs",
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
